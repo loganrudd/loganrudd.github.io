89e3ed4b3096fd04c005fc2716164b3eebdea50b
Logan Rudd <loganrudd@gmail.com>
2026-07-16 16:54:09 -0700

    Improve layout

--- js/common.js
+++ js/common.js
@@ -34,47 +34,6 @@ $(function() {
 
 
   /* =======================
-  // Reveal Image
-  ======================= */
-  var ww = window.innerWidth,
-    wh = window.innerHeight;
-
-  $(window).ready(function () {
-    $('body').waitForImages({
-      finished: function () {
-        setTimeout(function () {
-          $('.preloader').addClass('hide');
-
-          setTimeout(function () {
-            reveals();
-          }, 100);
-        }, 500);
-      },
-      waitForAll: true
-    });
-  });
-
-  function reveals() {
-    $(window).on('scroll', function () {
-      $(".article-box, .article-first, .post-image-box, .page-image-box, .post-body img, .page-body img, .recent-header").each(
-        function(i) {
-          var el_top = $(this).offset().top,
-            win_bottom = wh + $(window).scrollTop();
-
-          if (el_top < win_bottom) {
-            $(this)
-              .delay(i * 100)
-              .queue(function() {
-                $(this).addClass("reveal-in");
-              });
-          }
-        }
-      );
-    }).scroll();
-  }
-
-
-  /* =======================
   // Responsive Videos
   ======================= */
   $(".post-content, .page-content").fitVids({
